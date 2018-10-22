decaf42d24641921abe9eccac781156d455d1f69
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-10-22 12:40:54 -0400

    Modified restaurants-list class and other related css

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function listItems(n) {
   for (i = 0; i < rlist.length; i++) {
       rlist[i].style.display = "none"; 
   }
-   rlist[listIndex-1].style.display = "block"; 
+   rlist[listIndex-1].style.display = "inline"; 
   
 }
 
@@ -179,7 +179,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
   const ul = document.getElementById('restaurants-list');
   restaurants.forEach(restaurant => {
     ul.append(createRestaurantHTML(restaurant));
-      document.querySelectorAll('ul li')[0].style.display="block";
+      document.querySelectorAll('ul li')[0].style.display="inline";
   });
   addMarkersToMap();
 }
@@ -200,8 +200,8 @@ createRestaurantHTML = (restaurant) => {
  
    const src = image.appendChild(document.createElement('source'));
    src.srcset = DBHelper.imageUrlForRestaurant(restaurant);
-    src.media= 'min-width:701px';
-    src.sizes="20px";
+   // src.media= 'min-width:701px';
+    //src.sizes="20px";
     const srcimg = image.appendChild(document.createElement('img'));
     srcimg.src = DBHelper.imageUrlForRestaurant(restaurant);
     srcimg.className='restaurant-img';
@@ -225,10 +225,15 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+    
+  //const more = document.createElement('a');
+  
+  const dets = document.createElement('h3');
+  const more = dets.appendChild(document.createElement('a'));
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  //li.append(more)
+    li.append(dets);
 
   return li
 }
