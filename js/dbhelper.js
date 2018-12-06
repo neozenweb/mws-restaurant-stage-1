87378674aa8de052abdd6ad8997618b625f2ff66
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-12-05 21:06:11 -0500

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
    const port=80; // Change this to your server port
-    return `mws-restaurant-stage-1/data/restaurants.json`;
+    return `https://github.com/neozenweb/mws-restaurant-stage-1/blob/master/data/restaurants.json`;
   }
 
   /**
